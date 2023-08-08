7c16c395407d96b90a7c05fcf514c4782107ed08
Samuel <89222134+SamuelRedradix@users.noreply.github.com>
2023-08-08 16:24:05 +0200

    💄 font and css

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
     eleventyConfig.addPassthroughCopy("bundle.js");
+    eleventyConfig.addPassthroughCopy("https://fonts.googleapis.com/css2?family=Borel&display=swap");
+
   };
